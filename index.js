3801e5ed4c4bbe8280433f377ba20b203ce67423
Tanya <tanaybrown@gmail.com>
2022-05-21 11:35:42 -0700

    Holiday-Planner

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
+const vacationTodosArr = [
+    "book flights",
+    "find snorkle",
+]
+
+const vacationTodsoUl = document.getElementById("vacationTodosUl")
+const toDoImput = Dodocument.getElementById("todoInput")
+document.getElementById('form').addEventListener('sumbit', addTodo)
+
+function addTodo()  {
+    vacationTodosArr.push(todoInput.value);
+    console.log(vacationToDoArr)
+}
+
+function creatToDo(todo) {
+    const todoLabel = todo.split(" ").join("")
+    const li = document.createElement("li")
+    li.innerHTML = ` 
+                 <input type="checkbox" id="${todoLabel}" name="${todoLabel}">
+                 <label for="${todoLabel}">${todo}</label>`
+
+    vacationTodosUl.appendChild(li)
+}
+
+for (const i=0; i < vacationTodos.length; i++){
+    const todo = vacationTodosArr{i}
+    
+}
 
